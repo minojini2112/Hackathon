07631a55791209464876dabb994a72af63d73879
Minojini <minojini05@gmail.com>
2025-01-24 19:02:23 +0530

    added

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ const storage = new CloudinaryStorage({
 });
 
 const corsOptions = {
-  origin: "https://hackathon-tau-ashen.vercel.app/", 
+  origin: "https://hackathon-tau-ashen.vercel.app/ , http://localhost:5173/ ", 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
